dc494f391194208aad1b96ab1f08d194da74f2cd
HACKER <hacker@example.com>
2026-01-25 17:09:39 +0500

    update

--- backend/run_auth.bat
+++ backend/run_auth.bat
@@ -6,5 +6,5 @@ rem set TG_API_HASH=
 set FRONTEND_ORIGINS=https://node.e-qoldau.asia,http://localhost:5173
 set DB_PATH=%cd%\data.sqlite3
 
-py -3.11 -m uvicorn auth_api.main:app --host 127.0.0.1 --port 8001
+py -3.11 -m uvicorn auth_api.main:app --host 0.0.0.0 --port 8001
 pause
